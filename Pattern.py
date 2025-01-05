0334a1deb06c983b4479227d60d9ac668d41f343
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2025-01-05 13:00:07 +0000

    feat: add Policy class

--- Pattern.py
+++ Pattern.py
@@ -4,10 +4,23 @@ class Pattern:
         self.sources = data["sources"]
         self.sanitizers = data["sanitizers"]
         self.sinks = data["sinks"]
-        self.implicit = data["implicit"] == "yes"
-
+        self.implicit = data["implicit"]
+        
+        
+    def isSource(self, name):
+        return name in self.sources
+    
+    def isSanitizer(self, name):
+        return name in self.sanitizers
+    
+    def isSink(self, name):
+        return name in self.sinks
+        
+    def isImplicit(self):
+        return self.implicit == "yes"
+    
     def __repr__(self):
-        return f"Vulnerability | vulnerability: {self.vulnerability}: sources={self.sources} sanitizers={self.sanitizers} s__repr__ink={self.sinks} implicit={self.implicit}"
+        return f"Pattern | vulnerability: {self.vulnerability}: sources={self.sources} sanitizers={self.sanitizers} s__repr__ink={self.sinks} implicit={self.implicit}"
 
     def __eq__(self, other):
         return (
---
+++ Policy.py
@@ -0,0 +1,70 @@
+from MultiLabel import MultiLabel
+from Pattern import Pattern
+from Source import Source, Sanitizer, Sink
+
+class Policy:
+    def __init__(self, patterns):
+        for pattern in patterns:
+            if (not isinstance(pattern, Pattern)):
+                raise ValueError("Invalid pattern")
+        self.patterns = patterns
+                
+
+    def getVulnerabilities(self):
+        vulnerabilities = []
+        for pattern in self.patterns:
+            vulnerabilities += [pattern.vulnerability]
+        return vulnerabilities
+    
+    
+    def getPatternByName(self, name):
+        for pattern in self.patterns:
+            if (pattern.vulnerability == name):
+                return pattern
+        return None
+    
+    def getVulnerabilitiesBySource(self, source):
+        if (isinstance(source, Source)):
+            vulnerabilities = []
+            for pattern in self.patterns:
+                if (pattern.isSource(source.name)):
+                    vulnerabilities += [pattern.vulnerability]
+            return vulnerabilities
+        else:
+            raise ValueError("Invalid Source")
+    
+    def getVulnerabilitiesBySanitizer(self, sanitizer):
+        if (isinstance(sanitizer, Sanitizer)):
+            vulnerabilities = []
+            for pattern in self.patterns:
+                if (pattern.isSanitizer(sanitizer.name)):
+                    vulnerabilities += [pattern.vulnerability]
+            return vulnerabilities
+        else:
+            raise ValueError("Invalid Sanitizer")
+    
+    def getVulnerabilitiesBySink(self, sink):
+        if (isinstance(sink, Sink)):
+            vulnerabilities = []
+            for pattern in self.patterns:
+                if (pattern.isSink(sink.name)):
+                    vulnerabilities += [pattern.vulnerability]
+            return vulnerabilities
+        else:
+            raise ValueError("Invalid Sink")
+    
+    def illegalFlow(self, sinkName, multiLabel):
+        """
+        Returns the MultiLabel that contains only the labels (source, sanitizers[]) 
+        of the vulnerabilities that are illegal flows to the sink
+        """
+        if(isinstance(multiLabel, MultiLabel)):
+            newMultiLabel = MultiLabel()
+            for pattern in self.patterns:
+                if (pattern.isSink(sinkName)):
+                    m = multiLabel.getLabel(pattern.vulnerability)
+                    if (m is not None):
+                        newMultiLabel.addLabel(pattern.vulnerability, m)
+            return newMultiLabel
+        else:
+            raise ValueError("Invalid multiLabel")

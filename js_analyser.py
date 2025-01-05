b820911606a24ca35e8d604ac774c3c18681d542
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2025-01-05 11:21:00 +0000

    feat: Pattern class

--- js_analyser.py
+++ js_analyser.py
@@ -1,5 +1,27 @@
 import sys
 import esprima
+import json
+
+
+class Pattern:
+    def __init__(self, data):
+        self.vulnerability = data["vulnerability"]
+        self.sources = data["sources"]
+        self.sanitizers = data["sanitizers"]
+        self.sinks = data["sinks"]
+        self.implicit = data["implicit"] == "yes"
+
+    def __repr__(self):
+        return f"Vulnerability {self.vulnerability}: sources={self.sources} sanitizers={self.sanitizers} s__repr__ink={self.sinks} implicit={self.implicit}"
+
+    def __eq__(self, other):
+        return (
+            self.vulnerability == other.vulnerability
+            and self.sources == other.sources
+            and self.sanitizers == other.sanitizers
+            and self.sinks == other.sinks
+            and self.implicit == other.implicit
+        )
 
 
 def main():
@@ -14,8 +36,10 @@ def main():
     print(ast)
 
     with open(sys.argv[2], "r") as f:
-        patterns = f.read()
+        patterns = json.load(f)
+    patterns = [Pattern(data) for data in patterns]
+    print(patterns)
 
-    print(patterns)    
+
 if __name__ == "__main__":
     main()

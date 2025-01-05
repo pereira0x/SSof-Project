f74d18f2bd3135301a3de64f1181681585adc059
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2025-01-05 11:10:06 +0000

    feat: parse ast and patterns

--- js_analyser.py
+++ js_analyser.py
@@ -1,12 +1,21 @@
 import sys
+import esprima
+
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print(f"Usage: {sys.argv[0]} <slice> <patterns>", file=sys.stderr)
         return
 
     with open(sys.argv[1], "r") as f:
         slice = f.read()
 
+    ast = esprima.parseScript(slice, loc=True).toDict()
+    print(ast)
+
+    with open(sys.argv[2], "r") as f:
+        patterns = f.read()
+
+    print(patterns)    
 if __name__ == "__main__":
     main()

4e43cd07d08b359f47ecafab6249927e2a176615
José Pereira <jose.pereira@rnl.tecnico.ulisboa.pt>
2025-01-06 21:00:36 +0000

    feat: slightly traverse of an AST

--- Analyser.py
+++ Analyser.py
@@ -12,7 +12,7 @@ class Analyser(esprima.NodeVisitor):
 
     # TODO: if literal is a function call??
 
-    def visit_Script(self, node):
+    def visit_Script(self, node: nodes.Script):
         print("I am visiting a script")
 
         for op in node.body:
@@ -26,20 +26,34 @@ class Analyser(esprima.NodeVisitor):
             else:
                 print("Woah, what is this?")
 
-    def visit_Identifier(self, node):
+    def visit_Identifier(self, node: nodes.Identifier):
         print("I am visiting an identifier")
 
-    def visit_CallExpression(self, node):
+    def visit_simpleNodes(self, node):
+        if isinstance(node, nodes.CallExpression):
+            self.visit_CallExpression(node)
+        elif isinstance(node, nodes.ExpressionStatement):
+            self.visit_ExpressionStatement(node)
+        elif isinstance(node, nodes.AssignmentExpression):
+            self.visit_AssignmentExpression(node)
+        elif isinstance(node, nodes.Literal):
+            self.visit_Literal(node)
+        elif isinstance(node, nodes.Identifier):
+            self.visit_Identifier(node)
+        else:
+            print("Woah, what is this?")
+
+    def visit_CallExpression(self, node: nodes.CallExpression):
         print("I am visiting a call expression")
 
-    def visit_ExpressionStatement(self, node):
+    def visit_ExpressionStatement(self, node: nodes.ExpressionStatement):
         print("I am visiting an expression statement")
+        self.visit(node.expression)
 
-    def visit_AssignmentExpression(self, node):
+    def visit_AssignmentExpression(self, node: nodes.AssignmentExpression):
         print("I am visiting an assignment expression")
+        self.visit(node.left)
+        self.visit(node.right)
 
-    def visit_Literal(self, node):
+    def visit_Literal(self, node: nodes.Literal):
         print("I am visiting a literal")
-
-    def visit_Program(self, node):
-        print("I am visiting a program")

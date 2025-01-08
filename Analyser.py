9c5cc8a528cab3e05d8684608a035747b4b40f14
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2025-01-08 13:28:35 +0000

    feat: init if statement visitor

--- Analyser.py
+++ Analyser.py
@@ -19,7 +19,7 @@ class Analyser(esprima.NodeVisitor):
 
     def visit_Script(self, node: nodes.Script):
         print("I am visiting a script")
-
+        multiLabellings = [self.multiLabelling]
         for op in node.body:
 
             if isinstance(op, nodes.ExpressionStatement):
@@ -27,6 +27,16 @@ class Analyser(esprima.NodeVisitor):
 
             elif isinstance(op, nodes.AssignmentExpression):
                 self.visit_AssignmentExpression(op)
+            
+            elif isinstance(op, nodes.IfStatement):
+                new_multiLabellings = []
+                for multiLabelling in multiLabellings:
+                    if_multiLabelling, else_exists = self.visit_IFStatement(op, multiLabelling)
+                    new_multiLabellings += if_multiLabelling
+                if else_exists:
+                    multiLabellings = new_multiLabellings
+                else:
+                    multiLabellings += new_multiLabellings
 
             else:
                 print("Woah, what is this?")
@@ -67,9 +77,9 @@ class Analyser(esprima.NodeVisitor):
         multiLabelling.setMultiLabel(node.name, multiLabel)
         return multiLabel
 
-    def visit_simpleNodes(self, node, multiLabelling=None):
+    def visit_simpleNodes(self, node, multiLabelling=None, multiLabel_cond=None):
         if isinstance(node, nodes.CallExpression):
-            m = self.visit_CallExpression(node, multiLabelling)
+            m = self.visit_CallExpression(node, multiLabelling, multiLabel_cond)
         elif isinstance(node, nodes.Identifier):
             m = self.visit_Identifier(node, multiLabelling)
         elif isinstance(node, nodes.BinaryExpression):
@@ -81,7 +91,7 @@ class Analyser(esprima.NodeVisitor):
 
         return m
 
-    def visit_CallExpression(self, node: nodes.CallExpression, multiLabelling=None):
+    def visit_CallExpression(self, node: nodes.CallExpression, multiLabelling=None, multiLabel_cond=None):
         print("I am visiting a call expression")
         # a(b,1,2,3)
         if isinstance(node.callee, nodes.StaticMemberExpression):
@@ -93,7 +103,13 @@ class Analyser(esprima.NodeVisitor):
 
         for arg in node.arguments:
             multiLabel += self.visit_simpleNodes(arg, multiLabelling=multiLabelling)
-
+        
+        if multiLabel_cond is not None:
+            for vulnName in multiLabel_cond.labels:
+                if self.policy.getPatternByName(vulnName).isImplicit():
+                    label = multiLabel_cond.getLabel(vulnName).deepcopy()
+                    multiLabel.addLabel(vulnName, label)
+                    
         for vuln in multiLabel.labels:
             if self.policy.getPatternByName(vuln).isSanitizer(functionName):
                 sanitizer = Sanitizer(functionName, node.callee.loc.start.line)
@@ -118,7 +134,7 @@ class Analyser(esprima.NodeVisitor):
         self.visit(node.expression)
 
     def visit_AssignmentExpression(
-        self, node: nodes.AssignmentExpression, multiLabelling=None
+        self, node: nodes.AssignmentExpression, multiLabelling=None, multiLabel_cond=None
     ):
         print("I am visiting an assignment expression")
         """ self.visit(node.right)
@@ -129,6 +145,12 @@ class Analyser(esprima.NodeVisitor):
 
         # visit right side
         multiLabel = self.visit_simpleNodes(node.right, multiLabelling=multiLabelling)
+        
+        if multiLabel_cond is not None:
+            for vulnName in multiLabel_cond.labels:
+                if self.policy.getPatternByName(vulnName).isImplicit():
+                    label = multiLabel_cond.getLabel(vulnName).deepcopy()
+                    multiLabel.addLabel(vulnName, label)
         
         if multiLabel is not None:
             if isinstance(node.left, nodes.StaticMemberExpression):   # a.b() = something
@@ -174,6 +196,90 @@ class Analyser(esprima.NodeVisitor):
         multiLabelling.setMultiLabel(propertyName, propertyMultiLabel)
         
         return varMultiLabel + propertyMultiLabel
+      
+    def visit_IFStatement(self, node: nodes.IfStatement, multiLabelling=None, multiLabel_cond=None):
+        print("I am visiting an if statement")
+        
+        # multiLabelling_cond = multiLabelling.deepcopy()
+        if multiLabelling is None:
+            multiLabelling = self.multiLabelling
+        
+        multiLabelling_cond = multiLabelling.deepcopy()
+        
+        multiLabel = self.visit_simpleNodes(node.test, multiLabelling=multiLabelling_cond)
+        
+        if multiLabel_cond is not None:
+            multiLabel += multiLabel_cond
+        
+        if_multiLabellings = [multiLabelling_cond.deepcopy()]
+        
+        for op in node.consequent.body:
+            if isinstance(op, nodes.ExpressionStatement):
+              if isinstance(op.expression, nodes.AssignmentExpression):
+                print("AHJDHAHDKD")
+                for multiLabelling_aux in if_multiLabellings:
+                    self.visit_AssignmentExpression(op.expression, multiLabelling_aux, multiLabel)
+                    
+            elif isinstance(op, nodes.AssignmentExpression):
+                for multiLabelling_aux in if_multiLabellings:
+                    self.visit_AssignmentExpression(op, multiLabelling_aux, multiLabel)
+                  
+            elif isinstance(op, nodes.IfStatement):
+                new_multiLabellings = []
+                for multiLabelling_aux in if_multiLabellings:
+                    if_multiLabelling, else_exists = self.visit_IFStatement(op, multiLabelling_aux)
+                    new_multiLabellings += if_multiLabelling
+                '''
+                  if there is not an else statement,
+                  we need to account for the multilabels from the if-condition
+                '''               
+                if else_exists:
+                    if_multiLabellings = new_multiLabellings
+                else:
+                    if_multiLabellings += new_multiLabellings
+                    
+                # TODO: missing while node
+                
+        else_multiLabellings = [multiLabelling_cond.deepcopy()]
+        else_exists = False
+        if node.alternate:
+            print("I am visiting an else statement")
+            else_exists = True
+
+            for op in node.alternate.body:
+                print("IM DETECTING AN OP")
+                if isinstance(op, nodes.ExpressionStatement):
+                    print("LLLLL")
+                    if isinstance(op.expression, nodes.CallExpression):
+                        for multiLabelling_aux in else_multiLabellings:
+                            self.visit_CallExpression(op.expression, multiLabelling_aux)
+                    elif isinstance(op.expression, nodes.AssignmentExpression):
+                        print("AHJDHAHDKD")
+                        for multiLabelling_aux in else_multiLabellings:
+                            self.visit_AssignmentExpression(op.expression, multiLabelling_aux, multiLabel)          
+                elif isinstance(op, nodes.AssignmentExpression):
+                    print("DJDJDJ")
+                    for multiLabelling_aux in else_multiLabellings:
+                        self.visit_AssignmentExpression(op, multiLabelling_aux, multiLabel)
+                elif isinstance(op, nodes.IfStatement):
+                    print("DAJDHAK")
+                    new_multiLabellings = []
+                    for multiLabelling_aux in else_multiLabellings:
+                        if_multiLabelling, else_exists = self.visit_IFStatement(op, multiLabelling_aux)
+                        new_multiLabellings += if_multiLabelling
+                    '''
+                      if there is not an else statement,
+                      we need to account for the multilabels from the if-condition
+                    '''               
+                    if else_exists:
+                        else_multiLabellings = new_multiLabellings
+                    else:
+                        else_multiLabellings += new_multiLabellings
+                # TODO: missing while node
+                else:
+                    print("Woah, what is this?")
+                
+        return if_multiLabellings + else_multiLabellings, else_exists      
 
     def detectIllegalFlows(self, sink, multiLabel):
 
